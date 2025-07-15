d91ab99cd788665dcd1fbaad2190f9698de286ae
Chaitanya Raj <crsofficial7@gmail.com>
2025-07-16 01:03:34 +0530

    Vite env commit

--- src/Signup/Signup.jsx
+++ src/Signup/Signup.jsx
@@ -29,7 +29,7 @@ const Signup = () => {
       return;
     }
     try {
-      const res = await fetch(`${process.env.REACT_APP_API_URL}/signup`, {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,7 +18,7 @@ const Card = () => {
         }
     
         try {
-            const res = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
+            const res = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password }),
