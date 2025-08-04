46654258e0467ce48ad8fd1c63baa7907854ae92
Chaitanya Raj <crsofficial7@gmail.com>
2025-08-04 19:27:23 +0530

    Error debug

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 const verifyUser = async (req, res, next) => {
   console.log("Cookies received:", req.cookies);
